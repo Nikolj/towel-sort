d233734dca6d1d51e604ca64c6761a47080a03ef
Nikolj <frededdd@mail.ru>
2022-02-09 23:26:01 +0300

    feat: add index.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+
+
+module.exports = function towelSort (matrix) {
+  if (!matrix || matrix.length === 0) { return []; }
+    return [].concat(...matrix.map((array, index) => index % 2 == 0 ? array : array.reverse()));
+}
